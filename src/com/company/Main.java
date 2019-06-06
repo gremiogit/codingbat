4032a0b46b474fae94027b4916f5a3bf033a8748
Jason Ray <jason@jason.com>
2019-06-06 17:28:13 -0400

    Latest challenge complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -454,6 +454,7 @@ public class Main {
 //    lessBy10(1, 7, 11) → true
 //    lessBy10(1, 7, 10) → false
 //    lessBy10(11, 1, 7) → true
+//
 
     public boolean lessBy10(int a, int b, int c) {
         return Math.abs(a - b) >= 10 || Math.abs(a - c) >= 10 || Math.abs(b - c) >= 10;
